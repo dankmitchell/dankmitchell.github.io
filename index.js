448bccff7851ddb94a1844da77f593a9af507f5b
Dan Mitchell <dan@digitalreflow.co.uk>
2018-05-29 13:07:13 +0100

    Update homepage

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-var express = require('express');
+var express = require('express'),
+    sass    = require('node-sass'),
+    autoprefixer = require('express-autoprefixer'),
+    sassMiddleware = require('node-sass-middleware');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+app.use(
+  autoprefixer({
+    browsers: 'last 2 versions',
+    cascade: false
+  })
+);
+
+app.use(
+  sassMiddleware({
+    src: __dirname + '/sass',
+    dest: __dirname + '/public/stylesheets',
+    prefix:  '/stylesheets',
+    debug: true,
+  })
+);
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.render('pages/index');
