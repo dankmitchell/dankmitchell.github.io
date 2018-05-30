8b089d92d0f544fa47ca74bc80c38b4f637f594c
Dan Mitchell <dan@digitalreflow.co.uk>
2018-05-30 13:27:14 +0100

    Add work page

--- index.js
+++ index.js
@@ -46,6 +46,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/work', function(request, response) {
+  response.render('pages/work');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
